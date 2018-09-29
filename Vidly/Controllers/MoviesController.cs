3025e9fc6893d260015e65b15657394a451e27ac
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-09-28 22:05:15 -0400

    Add random page

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie {Name = "Shrek"};
+            
+            return View(movie);
+        }
+    }
+}
