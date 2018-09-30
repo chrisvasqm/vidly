2b26cb59dff2adf5ebc0122826db5c84c3ccf53a
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-09-29 20:10:04 -0400

    Add customers pages

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "Customer 1"},
+            new Customer {Id = 2, Name = "Customer 2"}
+        };
+
+        [Route("customers")]
+        public IActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = _customers.Find(c => c.Id == id);
+
+            if (customer == null)
+                return NotFound();
+
+            return View(customer);
+        }
+    }
+}
