83624c40a573671b26e58bdbab564a0817c3301a
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-10-10 23:58:26 -0400

    Display customer's membership type and birthday info

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -30,7 +30,7 @@ namespace Vidly.Controllers
         [Route("customers/details/{id}")]
         public IActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return NotFound();
