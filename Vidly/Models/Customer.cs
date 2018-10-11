b7e7318fa604c79d67efdb265c8f840d0b76147f
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-10-10 23:34:45 -0400

    Add birthday property

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
@@ -9,6 +10,7 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
+        public DateTime? Birthday { get; set; }
 
         public byte MembershipTypeId { get; set; }
         public MembershipType MembershipType { get; set; }
