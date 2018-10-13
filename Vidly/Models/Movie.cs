b1ee5ad4a2c7325325261400be3ec6991a40cab0
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-10-13 00:25:00 -0400

    Add date added for movie details

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -7,6 +7,7 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
 
         public Genre Genre { get; set; }
         public int GenreId { get; set; }
