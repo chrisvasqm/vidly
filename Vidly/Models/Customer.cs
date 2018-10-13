0bdd00ea6ed5c0817acc70eee67416910619334d
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-10-13 17:06:38 -0400

    Reformat code

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -6,14 +6,18 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        
         public bool IsSubscribedToNewsLetter { get; set; }
+        
         [Display(Name = "Date of Birth")]
         public DateTime? Birthday { get; set; }
 
         public byte MembershipTypeId { get; set; }
+        
         public MembershipType MembershipType { get; set; }
     }
 }
